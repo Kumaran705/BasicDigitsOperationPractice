fa1f3d918f2e6dd94b9115a405ef5fb087cf5c3d
Kumaran705 <btechece230914@smvec.ac.in>
2026-01-05 19:13:11 +0530

    Update and rename Nrevprime.java to Nrev.java

--- Nrev.java
+++ Nrev.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Nrevprime {
+public class Nrev {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
