3cc5ddd51b93606461c0308f1bd2592cae10e02f
Kumaran705 <kumar.19082005@gmail.com>
2026-01-10 11:51:13 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Armstrong{
+    boolean ArmstrongCheck(int n){
+      int num=n;
+      int sum=0; 
+      int digits=String.valuOf(n).length();
+      while(n>0){
+         int digit=n%10;
+          sum+=Math.pow(digit,digits);
+         n/=10;
+      }
+     return sum==num;
+          
+    }
+    public static void main(String args[]){
+         Scanner  sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         Armstrong a=new Armstrong();
+         System.out.println(a.ArmstrongCheck(n));
+    }
+}
+}
